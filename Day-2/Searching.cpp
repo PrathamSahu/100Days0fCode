a71bbc5c453e17aca3776b9797ed2ba72afafcc7
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-03 13:41:18 +0530

    Add files via upload

---
+++ Day-2/Searching.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void Insert(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+bool Search(node *head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    Insert(head, 1);
+    Insert(head, 2);
+    Insert(head, 3);
+    Insert(head, 4);
+    display(head);
+    cout<<Search(head,5)<<endl;
+    return 0;
+}
