fdd4d4b070154380379b6b650357cc224b6be56d
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-03 13:46:26 +0530

    Add files via upload

---
+++ Day-3/Deletion.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void Insert(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void DeleteAtHead(node *&head)
+{
+
+    node *todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
+void Delete(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (head->next == NULL)
+    {
+        DeleteAtHead(head);
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
+int main()
+{
+    node *head = NULL;
+    Insert(head, 1);
+    Insert(head, 2);
+    Insert(head, 3);
+    Insert(head, 4);
+    display(head);
+    Delete(head, 2);
+    display(head);
+    DeleteAtHead(head);
+    display(head);
+    return 0;
+}
