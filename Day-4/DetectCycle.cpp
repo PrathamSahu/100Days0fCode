dc207998c26fd2ce32aaf769fcaa969c1d66e7bd
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-04 20:19:22 +0530

    Add files via upload

---
+++ Day-4/DetectCycle.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void Insert(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void makeCycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startNode;
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+
+bool detectCycle(node *&head)
+
+{
+    node *slow = head;
+    node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void removeCycle(node* &head)
+{
+    node* slow=head;
+    node* fast=head;
+    do{
+        slow=slow->next;
+        fast=fast->next->next;
+    }while(slow!=fast);
+    fast=head;
+    while(slow->next!=fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next;
+    }
+    slow->next=NULL;
+}
+int main()
+{
+    node *head = NULL;
+    Insert(head, 1);
+    Insert(head, 2);
+    Insert(head, 3);
+    Insert(head, 4);
+    Insert(head, 5);
+    Insert(head, 6);
+    display(head);
+    makeCycle(head,3);
+    cout<<detectCycle(head)<<endl;
+    removeCycle(head);
+    cout<<detectCycle(head)<<endl;
+    return 0;
+}
