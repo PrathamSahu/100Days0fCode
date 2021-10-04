60b69e64a02ff9f4af04e007937265e3d5d4fd74
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-04 20:20:27 +0530

    Add files via upload

---
+++ Day-4/ReverseKnode.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void Insert(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+node* ReverseK(node* &head, int k)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    int count = 0;
+    while (currptr != NULL && count<k)
+    {
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+        prevptr=currptr;
+        currptr=nextptr;
+        count++;
+    }
+    if(nextptr!=NULL)
+    {
+    head->next=ReverseK(nextptr,k);
+    }
+    return prevptr;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    Insert(head, 1);
+    Insert(head, 2);
+    Insert(head, 3);
+    Insert(head, 4);
+    Insert(head, 5);
+    Insert(head, 6);
+    display(head);
+    node* newhead = ReverseK(head,2);
+    display(newhead);
+    return 0;
+}
