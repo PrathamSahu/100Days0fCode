ef533d3581611f93f8dc483a338fad2a3eaefcb5
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-05 18:35:14 +0530

    Add files via upload

---
+++ Day-5/OneUnique.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int unique(int arr[],int n)
+{
+    int xorSum=0;
+    for(int i=0;i<n;i++){
+        xorSum=xorSum^arr[i];
+    }
+    return xorSum;
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5,4,3,2,1};
+    cout<<unique(arr,9)<<endl;
+    return 0;
+}
