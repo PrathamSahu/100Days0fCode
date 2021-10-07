275351d0b925043ec71dd38779f2697e110008b5
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-07 19:01:08 +0530

    Add files via upload

---
+++ Day-7/Intersection.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void Insert(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+int Length(node *head)
+{
+    int l = 0;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        l++;
+    }
+    return l;
+}
+
+int Intersection(node *&head1, node *&head2)
+{
+    int l1 = Length(head1);
+    int l2 = Length(head2);
+    int d = 0;
+    node *ptr1;
+    node *ptr2;
+
+    if (l1 > l2)
+    {
+        d = l1 - l2;
+        ptr1 = head1;
+        ptr2 = head2;
+    }
+    else
+    {
+        d = l2 - l1;
+        ptr2 = head1;
+        ptr1 = head2;
+    }
+
+    while (d)
+    {
+        ptr1 = ptr1->next;
+        if (ptr1 == NULL)
+        {
+            return -1;
+        }
+        d--;
+    }
+
+    while (ptr1 != NULL && ptr2 != NULL)
+    {
+        if (ptr1 == ptr2)
+        {
+            return ptr1->data;
+        }
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+    return -1;
+}
+
+void createIntersect(node *head1, node *head2, int pos)
+{
+    node *temp1 = head1;
+    pos--;
+    while (pos--)
+    {
+        temp1 = temp1->next;
+    }
+
+    node *temp2 = head2;
+    while (temp2->next != NULL)
+    {
+        temp2 = temp2->next;
+    }
+
+    temp2->next = temp1;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head1 = NULL;
+    node *head2 = NULL;
+    Insert(head1, 1);
+    Insert(head1, 2);
+    Insert(head1, 3);
+    Insert(head1, 4);
+    Insert(head1, 5);
+    Insert(head1, 7);
+    Insert(head1, 8);
+    Insert(head2, 1);
+    Insert(head2, 2);
+    Insert(head2, 5);
+    Insert(head2, 7);
+    Insert(head2, 8);
+    createIntersect(head1, head2, 4);
+    display(head1);
+    display(head2);
+    cout << Intersection(head1, head2) << endl;
+    return 0;
+}
