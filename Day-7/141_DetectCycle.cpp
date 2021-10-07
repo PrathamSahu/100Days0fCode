e3413b5b592cb335771f7eac312b115de95b6589
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-07 18:52:21 +0530

    Add files via upload

---
+++ Day-7/141_DetectCycle.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+void makeCycle(ListNode *&head, int pos)
+{
+    ListNode *temp = head;
+    ListNode *startNode;
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+
+
+void Insert(ListNode *&head, int val)
+{
+
+    ListNode *n = new ListNode(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    ListNode *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(ListNode *head)
+{
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    ListNode *head = NULL;
+    Insert(head, 1);
+    Insert(head, 2);
+    Insert(head, 3);
+    Insert(head, 4);
+    Insert(head, 5);
+    Insert(head, 6);
+    display(head);
+    makeCycle(head,3);
+    Solution cycle;
+    cout<<cycle.hasCycle(head)<<endl;
+    return 0;
+}
