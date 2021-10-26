3409ced0bfee0e65706edf341c3ce66b5f91ad8a
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-26 20:07:26 +0530

    Add files via upload

---
+++ Day-26/SubString2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void substr(string s, string ans)
+{
+    if (s.length() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    char ch = s[0];
+    int code=ch;
+    string ros = s.substr(1);
+    substr(ros, ans);
+    substr(ros, ans + ch);
+    substr(ros, ans + to_string(code));
+}
+
+int main()
+{
+    string str = "AB";
+    substr(str, "");
+    return 0;
+}
