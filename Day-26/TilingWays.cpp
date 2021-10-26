33d19447d385cc4784aabdd39d2bbf9d32c54159
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-10-26 20:07:46 +0530

    Add files via upload

---
+++ Day-26/TilingWays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int tilingWays(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+
+    return tilingWays(n - 1) + tilingWays(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << tilingWays(n) << endl;
+    return 0;
+}
