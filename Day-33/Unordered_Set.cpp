205840beeb910188f9d41904e1ba93300292168f
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-11-02 13:15:42 +0530

    Add files via upload

---
+++ Day-33/Unordered_Set.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    unordered_set<int> s;
+    s.insert(3);
+    s.insert(4);
+    s.insert(1);
+    s.insert(2);
+    for (auto i : s)
+        cout << i << " ";
+    cout << endl;
+    cout << s.size() << endl;
+    s.erase(3);
+    for (auto i : s)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
