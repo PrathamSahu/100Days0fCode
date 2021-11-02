622e7c13676e247f1d2c818088dfd7a1833e12fd
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-11-02 13:14:48 +0530

    Add files via upload

---
+++ Day-33/Ordered_Multi_Set.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    multiset<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(3);
+    s.insert(3);
+    s.insert(4);
+    s.insert(4);
+    s.insert(4);
+    s.insert(4);
+    s.insert(4);
+    for (auto i : s)
+        cout << i << " ";
+    cout << endl;
+    cout << s.size() << endl;
+    s.erase(3);
+    s.erase(s.find(4));
+    for (auto i : s)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
