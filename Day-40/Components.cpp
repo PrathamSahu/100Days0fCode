14d3e563dc1a6cb55883b810cfc25c825533bde2
PRATHAM SAHU <80022922+PrathamSahu@users.noreply.github.com>
2021-11-09 17:01:51 +0530

    Add files via upload

---
+++ Day-40/Components.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> vis;
+vector<vector<int>> adj;
+vector<int> component;
+
+int get_comp(int idx)
+{
+    if (vis[idx])
+        return 0;
+    vis[idx] = true;
+    int ans = 1;
+    for (auto i : adj[idx])
+    {
+        if (!vis[i])
+        {
+            ans += get_comp(i);
+            vis[i] = true;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    int n, m;
+    cin >> n >> m;
+
+    adj = vector<vector<int>>(n);
+    vis = vector<bool>(n);
+
+    int x, y;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> x >> y;
+
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            component.push_back(get_comp(i));
+        }
+    }
+    for (auto i : component)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
